f49b404398bb4d2fe35907c23f2d6c618185e781
wangbin <wangbin20100729@163.com>
2019-11-26 18:20:56 +0800

    添加数据库读写方法 添加保留小数位数的思路

--- src/main/scala/com/wangbin/clouddemo/util/TestRowNumber.scala
+++ src/main/scala/com/wangbin/clouddemo/util/TestRowNumber.scala
@@ -8,51 +8,70 @@ import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.expressions.Window
 
-case class order(id:Int,name:String,price:Double,nums:Int)
+case class order(id: Int, name: String, price: Double, nums: Int)
+
 object TestRowNumber {
   def main(args: Array[String]): Unit = {
     val spark: SparkSession = SparkBuilder.builder(true)
     import spark.implicits._
     val df: DataFrame = Seq(
       order(1, "张䶮", 12.12, 3),
-      order(1, "李䶮飞", 45.2, 3),
-      order(1, "李䶮飞飞", 45.2, 3),
-      order(1, "张李明䶮", 56.2, 1),
-      order(2, "hongbo李", 45.12, 6),
-      order(2, "hongbo", 33.2, 3),
-      order(2, "hong李bo", 45.23, 6)
+      order(2, "李䶮飞", 45.2, 3),
+      order(5, "李䶮飞飞", 45.2, 3),
+      order(3, "张李明䶮", 56.2, 1),
+      order(6, "hongbo李", 45.12, 6),
+      order(7, "hongbo", 33.2, 3),
+      order(4, "hong李bo", 45.23, 6)
     ).toDF()
+    df.createOrReplaceTempView("order")
+    //    spark.sql("select id,name,price,nums,cast(price*nums as decimal(18,4)) as sum from order").show()  //小数精度
+
+    df.selectExpr("id",
+      "name",
+      "price",
+      "nums",
+      "cast(price*nums as decimal(18,4)) as sum"
+    ).show()
+
+
+
+//    df = df.selectExpr("round(money,2) as money" ,"created_ts","updated_ts");
+//    df = df.selectExpr("cast(money as decimal(20,2)) as money" ,"created_ts","updated_ts");
+
+
+    //    df.sort("id").show()
+
+
     val schema = df.schema.fieldNames
-//    df.repartition(1).select(concat_ws("|",schema.map(df(_)):_*) as schema.mkString("|")).write.option("charset", "iso-8859-1").option("sep", "|").mode("overwrite").text("D:\\IdeaProjects\\clouddemo\\src\\main\\resources\\df.txt")
+    //    df.repartition(1).select(concat_ws("|",schema.map(df(_)):_*) as schema.mkString("|")).write.option("charset", "iso-8859-1").option("sep", "|").mode("overwrite").text("D:\\IdeaProjects\\clouddemo\\src\\main\\resources\\df.txt")
     println(schema)
-    df.filter(! $"name".like("李%")).show()
+    //    df.filter(! $"name".like("李%")).show()
 
-//println(schema)
-//    df.repartition(1)
-//      .select(concat_ws("|",schema.map(df(_)):_*) as schema.mkString("|"))
-////      .write.option("charset", "iso-8859-1")
-////      .write.option("charset", "iso-8859-1")
-//      .write.option("charset", "GBK")
-//      .option("sep", "|")
-//      .mode("overwrite")
-//      .text("D:\\IdeaProjects\\clouddemo\\src\\main\\resources\\df.txt")
-//    val df2 = df.withColumn("row_seq",row_number().over(Window.partitionBy($"name").orderBy($"price".desc)))
-//      .filter($"row_seq"===1)
-//      .show()
-//
-//
-//    df2.select("id","name","price").show()
+    //println(schema)
+    //    df.repartition(1)
+    //      .select(concat_ws("|",schema.map(df(_)):_*) as schema.mkString("|"))
+    ////      .write.option("charset", "iso-8859-1")
+    ////      .write.option("charset", "iso-8859-1")
+    //      .write.option("charset", "GBK")
+    //      .option("sep", "|")
+    //      .mode("overwrite")
+    //      .text("D:\\IdeaProjects\\clouddemo\\src\\main\\resources\\df.txt")
+    //    val df2 = df.withColumn("row_seq",row_number().over(Window.partitionBy($"name").orderBy($"price".desc)))
+    //      .filter($"row_seq"===1)
+    //      .show()
+    //
+    //
+    //    df2.select("id","name","price").show()
 
 
-//    val dataframe = spark.read.option("header", true)
-//      .option("inferSchema",false)
-//      .option("ignoreLeadingWhiteSpace", true)
-//      .option("ignoreTrailingWhiteSpace", true)
-//      .option("sep", "|")
-//      .csv("D:\\IdeaProjects\\clouddemo\\src\\main\\resources\\test.csv")
-//    dataframe.printSchema()
-//    dataframe.filter(to_timestamp($"date","yyyy/MM/dd") > new Timestamp(parse2Date("2019-02-28","yyyy-MM-dd").getTime)).show()
-
+    //    val dataframe = spark.read.option("header", true)
+    //      .option("inferSchema",false)
+    //      .option("ignoreLeadingWhiteSpace", true)
+    //      .option("ignoreTrailingWhiteSpace", true)
+    //      .option("sep", "|")
+    //      .csv("D:\\IdeaProjects\\clouddemo\\src\\main\\resources\\test.csv")
+    //    dataframe.printSchema()
+    //    dataframe.filter(to_timestamp($"date","yyyy/MM/dd") > new Timestamp(parse2Date("2019-02-28","yyyy-MM-dd").getTime)).show()
 
 
     //    dataframe.select("id","name").show()
--- src/main/scala/com/wangbin/clouddemo/util/writeOracle.scala
+++ src/main/scala/com/wangbin/clouddemo/util/writeOracle.scala
@@ -1,18 +1,20 @@
 package com.wangbin.clouddemo.util
 
 
-
 import java.sql.DriverManager
 import java.util.Properties
 
 import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.execution.datasources.jdbc.JDBCOptions
 import org.apache.spark.sql.types.{DateType, DoubleType, IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.{DataFrame, Row, SQLContext, SaveMode, SparkSession}
 
 object writeOracle {
 
   val spark: SparkSession = SparkBuilder.builder()
+
   import spark.implicits._
+
   val person1: DataFrame = Seq(Person(1, "zhangsan", "zhangbj"),
     Person(2, "lisi", "libj"),
     Person(3, "wangwu", "wangsh"),
@@ -37,15 +39,71 @@ object writeOracle {
 
   def saveSummary(df: DataFrame, sqlContext: SQLContext): Unit = {
     //DateTypes.createStructField()
-    val schema= StructType(List(StructField("mon_th",IntegerType, true)
+    val schema = StructType(List(StructField("mon_th", IntegerType, true)
       , StructField("current_date", DateType, true)
       , StructField("amount", DoubleType, true)
       , StructField("account_code", StringType, true)
       , StructField("summary_code", StringType, true)
       , StructField("project", StringType, true)
     ))
-//    val df: DataFrame = sqlContext.createDataFrame(rowrdd, schema)
-    df.write.mode(SaveMode.Append).jdbc(url,"表名",properties)
+    //    val df: DataFrame = sqlContext.createDataFrame(rowrdd, schema)
+    df.write.mode(SaveMode.Append).jdbc(url, "表名", properties)
   }
 
-}
+  /**
+   * 连接数据库工具类
+   */
+
+    /**
+     * 读取Oracle数据库
+     *
+     * @param spark
+     * @param dbtable
+     * @return
+     */
+    def readPMSGSOracleData(spark: SparkSession, dbtable: String) = {
+      val data: DataFrame = spark.read.format("jdbc")
+        .option("url", "jdbc:oracle:thin:@//10.212.242.XXX:11521/pmsgs")
+        .option("dbtable", dbtable)
+        .option("user", "qyw")
+        .option("password", "XXX")
+        .load()
+      data
+    }
+
+    /**
+     * 读取mysql数据库
+     *
+     * @param spark
+     * @param dbtable
+     * @return
+     */
+    def readUSAPPData(spark: SparkSession, dbtable: String) = {
+      val data: DataFrame = spark.read.format("jdbc")
+        .option("url", "jdbc:mysql://21.76.120.XX:3306/us_app")
+        .option("dbtable", dbtable)
+        .option("user", "root")
+        .option("password", "XXX")
+        .load()
+      data
+    }
+
+    /**
+     * 将数据写入到Mysql数据库(追加写入)
+     *
+     * @param result
+     * @param dbtable
+     */
+    def writeData(result: DataFrame, dbtable: String): Unit = {
+      result.write.mode(SaveMode.Append).format("jdbc")
+        .option(JDBCOptions.JDBC_URL, "jdbc:mysql://21.76.120.XX:3306/us_app?rewriteBatchedStatement=true")
+        .option("user", "root")
+        .option("password", "XXX")
+        .option(JDBCOptions.JDBC_TABLE_NAME, dbtable)
+        .option(JDBCOptions.JDBC_TXN_ISOLATION_LEVEL, "NONE") //不开启事务
+        .option(JDBCOptions.JDBC_BATCH_INSERT_SIZE, 500) //设置批量插入
+        .save()
+    }
+  }
+
+
